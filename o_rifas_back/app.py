decc00fdb8140fbe58eccfe4d93a83ed500e5ef7
Mateus Santos <a.mateussm1.5@gmail.com>
2023-06-22 22:17:09 -0300

    Enable CORS for all routes

--- o_rifas_back/app.py
+++ o_rifas_back/app.py
@@ -1,15 +1,13 @@
 from api_config import (
     config_app
 )
-from services.google_sheets import (
-    get_info_as_list
-)
 from controllers.table import (
     blp
 )
 
 from flask import Flask
 from flask_smorest import Api
+from flask_cors import CORS
 
 
 __version__: str = '0.1.0'
@@ -26,7 +24,8 @@ def create_app() -> Flask:
     config_app(app)
     api = Api(app)
 
-    
     api.register_blueprint(blp)
+
+    CORS(app)
 
     return app
